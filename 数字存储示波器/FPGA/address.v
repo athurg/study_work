031818209672b378d7abc79dc4ec1fba993cda54
athurg <athurg@f652d66d-742e-b847-b89e-e1c595eb4ae3>
2009-05-07 15:07:33 +0000

    增加屏幕移动功能，修正地址生成器
    git-svn-id: file:///media/CODE/git/svnroot/athurg@26 f652d66d-742e-b847-b89e-e1c595eb4ae3

--- 数字存储示波器/FPGA/address.v
+++ 数字存储示波器/FPGA/address.v
@@ -3,6 +3,7 @@
 输入：
 	clock:标准时钟
 	sample_type:采样类型，取1为连续采样，取0为单次采样
+	move:显示起始地址移动信号
 输出：
 	ram_addr:读写操作的RAM地址
 说明：
@@ -10,18 +11,32 @@
 	当连续采样时，RAM地址从0递增到399（即取两帧400个点）；
 */
 module address(
-		input clock,sample_type,
-		output reg [8:0] ram_addr
+		input clock, sample_type, key_right, key_left,
+		output reg [8:0] rd_addr, wr_addr
 	);
 
-	reg [8:0] max_addr;
-	always @(sample_type) begin
-		if(sample_type)	max_addr=9'd399;
-		else	max_addr = 9'd199;
-	end
+	reg [7:0] start_addr;
+	reg [8:0] end_addr, wr_depth;
+
 	
+	/*
+		RAM读写地址产生进程
+	写地址比读地址大1，先写后读相差一个时钟周期；
+	实现方式：在进程增加写地址前，先将写地址赋给读地址
+		屏幕显示波形左右移动键处理进程
+	说明：
+		左（右）移分别对应显示起始地址减（增）
+	*/
 	always @(posedge clock) begin
-		if(ram_addr >= max_addr)	ram_addr=8'd0;
-		else	ram_addr = ram_addr + 8'd1;
+		//根据采样类型，确定采样RAM的深度
+		wr_depth = sample_type ? 9'd399 : 9'd199;
+		wr_addr = (wr_addr < wr_depth) ? (wr_addr + 9'd1) : 9'd0;
+
+		if({key_right,key_left}) begin	//处理移动键
+			if(key_left)	start_addr = (start_addr > 8'd0) ? (start_addr -1) : 8'd0;
+			else	start_addr = (start_addr < 8'd200) ? (start_addr +1) : 8'd200;
+			end_addr = start_addr + 8'd200;
+		end
+		rd_addr =(rd_addr < end_addr)? (rd_addr + 1) : start_addr;
 	end
 endmodule
